c0eeefd65d543cfdeb5519d9f8655a15e715326c
CookiesCream1 <luky.pospa04@gmail.com>
2024-04-04 21:54:57 +0200

    fixing the five minute fuckery

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,5 +1,6 @@
-import GoogleProvider from 'next-auth/providers/google'
 import { NuxtAuthHandler } from '#auth'
+import GoogleProvider from 'next-auth/providers/google'
+import { useDbClient } from '~/composables/useDbClient'
 
 export default NuxtAuthHandler({
   secret: process.env.auth_secret,
@@ -10,7 +11,7 @@ export default NuxtAuthHandler({
       clientSecret: process.env.google_client_secret
     })
   ],
-  session: {
+   session :  {
     strategy: 'jwt'
   },
   callbacks: {
@@ -21,13 +22,20 @@ export default NuxtAuthHandler({
         ...account
       }
     },
-    session ({ session, token }) {
-      session.user = {
-        ...session.user,
-        ...token
-      }
+      async session ({ session, token }) {
+        const id = token.id
+        const client = await useDbClient()
+        const rows = await client.query('select count() from users where user_id = ?', [id])
+        if (rows[0]['count()'] === BigInt(0)) {
+          await client.query('insert into users (user_id) values (?)', [id])
+        } 
+        session.user = {
+          ...session.user,
+          ...token
+        }
 
       return session
     }
   }
-})
+},
+)
